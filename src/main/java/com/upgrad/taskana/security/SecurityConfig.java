6c3255247feb5fbf6e3f1d86a30625a56f65d1e4
Ishwar Soni <ishwar.soni@upgrad.com>
2020-08-06 07:47:39 +0530

    in memory authentication

--- src/main/java/com/upgrad/taskana/security/SecurityConfig.java
+++ src/main/java/com/upgrad/taskana/security/SecurityConfig.java
@@ -1,11 +1,25 @@
 package com.upgrad.taskana.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()
+				.withUser("ishwar")
+					.password("{noop}soni")
+					.authorities("ROLE_USER")
+				.and()
+				.withUser("parth")
+					.password("{noop}aksh")
+					.authorities("ROLE_USER");
+	}
 
 }
